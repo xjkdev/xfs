fa9e41df17504ed1acb6992399d115ef98aeb798
非栓 铨 <a502230347@qq.com>
2019-06-25 11:28:06 +0800

    deal with some error

--- xfs/include/permit.h
+++ xfs/include/permit.h
@@ -4,13 +4,10 @@
 #define GROUPMAXSIZE 500 
 
 #include <stdint.h>
-struct usr ;
-struct group;
+// typedef struct usr usr ;
+// typedef struct group group;
 //all need to be define in main function
-extern usr usr_list[USRMAXSIZE];
-extern group   group_list[GROUPMAXSIZE];
-extern int16_t uid_auto_increase;
-extern int16_t gid_auto_increase;
+
 
 //correct return uid else return -1
 int32_t creat_usr(char *usr_name,char* passwd);
@@ -49,3 +46,7 @@ typedef struct group
 
 }group;
 
+extern usr usr_list[USRMAXSIZE];
+extern group   group_list[GROUPMAXSIZE];
+extern int16_t uid_auto_increase;
+extern int16_t gid_auto_increase;
